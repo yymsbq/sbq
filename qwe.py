f6f1af1f1e252960aeb98be754b314d3def6aa40
yymsbq <42725994+yymsbq@users.noreply.github.com>
2018-08-27 16:36:30 +0800

    杨一名的8.27作业

---
+++ qwe.py
@@ -0,0 +1,45 @@
+#第一题
+#c=eval(input('c'))
+#print((9/5)*c+32)
+
+#第二题
+#r=eval(input('r' ))
+#l=eval(input('l'))
+#print('area=',r*r*3.141)
+#print('volume=',r*r*3.141*l)
+
+#第三题
+#feet=eval(input('feet'))
+#print(feet,"英尺=",feet*0.305,"米")
+
+
+#第四题
+
+#m=eval(input('请输入水量'))
+#i=eval(input('请输入初始温度'))
+#f=eval(input('请输入最终温度'))
+#print("能量：",m*(f-i)*4184)
+
+
+#a,b  = input('do something').split(',')
+#print(a,b)
+#q='a c f f'
+#print(q.split(' '))
+#第五题
+#a,b  = eval(input('差额，年利率'))
+#print('利息=',a*(b/1200))
+
+#第六题
+#a,b,t = eval(input('初始速度，末速度,时间'))
+#print(round((b-a)/t,4))
+
+#第七题
+
+
+#第八题
+#n=eval(input('输入一个0到1000之间的整数'))
+#a=n//100
+#b=(n-a*100)//10
+#c=n-a*100-b*10
+#print(a+b+c)
+
